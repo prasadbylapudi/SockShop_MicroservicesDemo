912968d1c92c3f608693008fec82744ce1f92bfd
prasadbylapudi <myemail>
2023-11-12 02:35:40 +0530

    feat: 🎸 added test-cases

--- tests/user-cart.test.js
+++ tests/user-cart.test.js
@@ -1,33 +1,72 @@
+const axios = require('axios');
 
-function add(a, b) {
-  return a + b;
-}
-// Test case 1: Adding two positive numbers
-test('adds 2 + 3 to equal 5', () => {
-  expect(add(2, 3)).toBe(5);
+// describe('Integration Tests for /register', () => {
+//   test('POST /register - Successful registration', async () => {
+//     const userData = {
+//       username: 'gary2',
+//       password: 'gary2',
+//       email: 'gary2@gmail.com', 
+//     };
+
+//     const response = await axios.post("http://host.docker.internal/register", userData);
+//     expect(response.data).toHaveProperty('id');
+
+//   });
+// });
+
+describe('Integration Tests for /customers', () => {
+  test('GET /customers - Valid response', async () => {
+    const response = await axios.get("http://host.docker.internal/customers");
+
+    expect(response.status).toBe(200);
+  });
 });
 
-// Test case 2: Adding a positive and a negative number
-test('adds 2 + (-3) to equal -1', () => {
-  expect(add(2, -3)).toBe(-1);
+
+describe('Integration Tests for /customers/{id}', () => {
+  test('GET /customers/{id} - Check user properties', async () => {
+    const userId = '57a98d98e4b00679b4a830b5';
+
+    const response = await axios.get(`http://host.docker.internal/customers/${userId}`);
+
+    expect(response.status).toBe(200);
+  });
 });
 
-// Test case 3: Adding two negative numbers
-test('adds (-2) + (-3) to equal -5', () => {
-  expect(add(-2, -3)).toBe(-5);
+
+// describe('Integration Tests for /orders', () => {
+//   test('GET /orders - User not logged in', async () => {
+
+//     const response = await axios.get('http://host.docker.internal/orders');
+//     // expect(response.status).toBe(401); 
+//     expect(response.data).toHaveProperty('message', 'User not logged in.');
+//     // expect(responseData).toHaveProperty('error', expect.any(Object));
+//   });
+// });
+
+describe('Integration Tests for /catalogue', () => {
+  test('GET /catalogue - Check response status', async () => {
+    try {
+      const response = await axios.get('http://host.docker.internal/catalogue');
+
+      // Assuming a successful response
+      expect(response.status).toBe(200);
+    } catch (error) {
+      // If the request fails, the test should fail
+      expect(error.response.status).toBeUndefined();
+    }
+  });
 });
 
-// Test case 4: Adding zero to a number
-test('adds 0 + 7 to equal 7', () => {
-  expect(add(0, 7)).toBe(7);
-});
-
-// Test case 5: Adding two zeros
-test('adds 0 + 0 to equal 0', () => {
-  expect(add(0, 0)).toBe(0);
-});
-
-// Test case 6: Adding a positive number and zero
-test('adds 4 + 0 to equal 4', () => {
-  expect(add(4, 0)).toBe(4);
+describe('Integration Tests for /catalogue/{id}', () => {
+  test('GET /catalogue/{id} - Check response status', async () => {
+    const itemId = 'a0a4f044-b040-410d-8ead-4de0446aec7e'; 
+    try {
+      const response = await axios.get(`http://host.docker.internal/catalogue/${itemId}`);
+      expect(response.status).toBe(200);
+    } catch (error) {
+      // If the request fails, the test should fail
+      expect(error.response.status).toBeUndefined();
+    }
+  });
 });
